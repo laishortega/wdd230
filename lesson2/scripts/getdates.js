35a071493931bc7c067c53787cbbf9dc59bfcac3
Use your Github username <Enter your email address>
2023-09-23 20:49:56 -0600

    Design Principle

---
+++ lesson2/scripts/getdates.js
@@ -0,0 +1,4 @@
+var current_year = new Date().getFullYear();
+
+document.getElementById("footer_1").innerHTML = "&copy" + current_year + " | Laish Kish T. Ortega | Philippines";
+document.getElementById("lastModified").innerHTML = "Last Modified: " + document.lastModified;
