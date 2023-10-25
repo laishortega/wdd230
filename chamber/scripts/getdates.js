c8130ac84fc5fc4f354cced98bb8fdb4a177e9ae
Use your Github username <Enter your email address>
2023-10-25 07:05:12 -0600

    chambersite

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,4 @@
+var current_year = new Date().getFullYear();
+
+document.getElementById("footer_1").innerHTML = "&copy" + current_year + " | Laish Kish T. Ortega | WDD 230";
+document.getElementById("lastModified").innerHTML = "Last Modified: " + document.lastModified;
