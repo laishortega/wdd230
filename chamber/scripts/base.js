fb6214bc4024fe65ecffa6fd2a54ba16bdfb226d
Use your Github username <Enter your email address>
2023-11-18 19:55:03 -0700

    JSON DATA

--- chamber/scripts/base.js
+++ chamber/scripts/base.js
@@ -32,3 +32,5 @@ document.querySelector("#dark-btn").addEventListener("click", () => {
     document.querySelector("#dark-btn").classList.toggle("dark");
     document.querySelector("body").classList.toggle("dark");
 });
+
+ last element in the document with the id of span
